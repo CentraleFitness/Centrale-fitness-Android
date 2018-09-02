eec68c34c89141478bb380e8c44187bd39695fe9
remy Villules <remy.villulles@gmail.com>
2018-09-02 17:55:27 +0100

    Rajout de firebase pour l'analytic Correction d'un souci de non ouverture de l'activity feedback

--- mobile/src/main/java/com/fitness/centrale/mobile/activities/profile/ProfileActivity.java
+++ mobile/src/main/java/com/fitness/centrale/mobile/activities/profile/ProfileActivity.java
@@ -120,7 +120,7 @@ public class ProfileActivity extends AppCompatActivity {
              @Override
              public void onClick(View v) {
                 Intent intent = new Intent(ProfileActivity.this, FeedBackActivity.class);
-
+                startActivity(intent);
              }
          });
 
